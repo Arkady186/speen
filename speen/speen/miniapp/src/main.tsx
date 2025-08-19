831e2c17c57bdb0840782b9e4876ea7383ec2960
Arkady <arkady@example.com>
2025-08-19 16:03:13 +0300

    feat: replace with miniapp loader

---
+++ speen/speen/miniapp/src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { WheelLoader } from './wheel/WheelLoader'
+import './styles.css'
+
+const container = document.getElementById('root')!
+const root = createRoot(container)
+
+root.render(
+	<React.StrictMode>
+		<WheelLoader />
+	</React.StrictMode>
+)
+
