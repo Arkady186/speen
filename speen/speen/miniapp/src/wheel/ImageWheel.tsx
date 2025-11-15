e0429ed76392047d8229f41f8407326c46f40990
Arkady <arkady@example.com>
2025-11-16 00:16:19 +0300

    fix: move plus button slightly down and right

--- speen/speen/miniapp/src/wheel/ImageWheel.tsx
+++ speen/speen/miniapp/src/wheel/ImageWheel.tsx
@@ -108,8 +108,8 @@ export const ImageWheel = React.forwardRef<ImageWheelRef, ImageWheelProps>(({ si
         const pointerAzimuth = Math.atan2(py - cy, px - cx) // рад
         const plusAngle = pointerAzimuth + Math.PI + (Math.PI / 18) // противоположное направление, смещение правее на 10 градусов
         const r = size * 0.48 // радиус размещения плюса - чуть ниже, но все еще на колесе
-        const x = cx + r * Math.cos(plusAngle)
-        const y = cy + r * Math.sin(plusAngle)
+        const x = cx + r * Math.cos(plusAngle) + 3 // смещение правее на 3px
+        const y = cy + r * Math.sin(plusAngle) + 5 // смещение вниз на 5px
         return { x, y }
     }
 
